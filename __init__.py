425409b89de683d45f3d5457a0cc8c6ba057c429
Andrew <lyfeshomebrew@gmail.com>
2019-10-03 01:54:54 -0700

    fix import in init

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 # __init__.py
 from .customwidgets import *
 from .searchbox import searchBox 
-from .storeappsquare import storeAppSquare
 from .framework import activeFrame
 from .framemanager import frameManager
 from .progbar import progBar
