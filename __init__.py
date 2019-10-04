081444c6737fe21434aec89d69ed412886d29a97
Andrew <lyfeshomebrew@gmail.com>
2019-10-03 20:35:05 -0700

    add image sharer to init

--- __init__.py
+++ __init__.py
@@ -6,3 +6,4 @@ from .framemanager import frameManager
 from .progbar import progBar
 from .progressframe import progressFrame
 from .themedscrollingtext import themedScrollingText
+from .tk_image_sharer import icon_dict
